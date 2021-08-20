c31e04277bb1923dbc7761a55924510627d3800f
langdonglei <langdonglei@icloud.com>
2021-08-20 18:29:35 +0800

    m

--- pages/page1/page1.vue
+++ pages/page1/page1.vue
@@ -1,13 +1,9 @@
 <template>
-	<view class="u-demo">
-		<view class="u-demo-wrap">
-			<view class="u-demo-title">演示效果</view>
 			<view class="u-demo-area">
-				<u-grid :col="col" @click="click" v-if="!isSwiper" :border="border">
-					<u-grid-item name="item1" :index="0" @click="itemClick">
-						<u-badge count="9" :offset="[col == 3 ? 20 : 14, col == 3 ? 30 : 20]"></u-badge>
-						<u-icon name="photo" :size="46"></u-icon>
-						<view class="grid-text">图片</view>
+				<u-grid>
+					<u-grid-item name="item1" :index="10" @click="handle_grid_item_click">
+						<u-icon name="photo" size="46"></u-icon>
+						<text class="grid-text">图片</text>
 					</u-grid-item>
 					<u-grid-item :index="1">
 						<u-icon name="lock" :size="46"></u-icon>
@@ -34,7 +30,8 @@
 						<view class="grid-text">音量</view>
 					</u-grid-item>
 					<u-grid-item :index="6">
-						<image src="https://cdn.uviewui.com/uview/grid/hot6.png" style="width: 44rpx;height: 44rpx;top:0;right:0;" class="badge-icon"></image>
+						<image src="https://cdn.uviewui.com/uview/grid/hot6.png"
+							style="width: 44rpx;height: 44rpx;top:0;right:0;" class="badge-icon"></image>
 						<u-icon name="trash" :size="46"></u-icon>
 						<view class="grid-text">回收站</view>
 					</u-grid-item>
@@ -47,7 +44,7 @@
 						<view class="grid-text">购物车</view>
 					</u-grid-item>
 				</u-grid>
-				<swiper class="swiper" v-else @change="change">
+				<swiper class="swiper" @change="change">
 					<swiper-item>
 						<u-grid :border="border" :col="col" @click="click" hover-class="hover-class">
 							<u-grid-item v-for="(item, index) in list" :index="index" :key="index">
@@ -73,31 +70,7 @@
 						</u-grid>
 					</swiper-item>
 				</swiper>
-				<view class="indicator-dots" v-if="isSwiper">
-					<view class="indicator-dots-item" :class="[current == 0 ? 'indicator-dots-active' : '']">
-					</view>
-					<view class="indicator-dots-item" :class="[current == 1 ? 'indicator-dots-active' : '']">
-					</view>
-					<view class="indicator-dots-item" :class="[current == 2 ? 'indicator-dots-active' : '']">
-					</view>
-				</view>
 			</view>
-		</view>
-		<view class="u-config-wrap">
-			<view class="u-config-item">
-				<view class="u-item-title">是否显示边框</view>
-				<u-subsection :list="['是', '否']" @change="borderChange"></u-subsection>
-			</view>
-			<view class="u-config-item">
-				<view class="u-item-title">是否可滑动</view>
-				<u-subsection current="1" :list="['是', '否']" @change="isSwiperChange"></u-subsection>
-			</view>
-			<view class="u-config-item">
-				<view class="u-item-title">修改列数</view>
-				<u-subsection :list="['3', '4']" @change="colChange"></u-subsection>
-			</view>
-		</view>
-	</view>
 </template>
 
 <script>
@@ -121,7 +94,7 @@
 			colChange(index) {
 				this.col = index == 0 ? 3 : 4;
 			},
-			click(index) {
+			handle_grid_click(index) {
 				this.$refs.uToast.show({
 					title: `点击了第${index + 1}宫格`,
 					type: 'warning'
@@ -131,7 +104,7 @@
 				this.current = e.detail.current;
 			},
 			// 针对单个grid-item的事件
-			itemClick(index) {
+			handle_grid_item_click(index) {
 				// console.log(index);
 			}
 		}
@@ -139,38 +112,38 @@
 </script>
 
 <style scoped lang="scss">
-.grid-text {
-	font-size: 28rpx;
-	margin-top: 4rpx;
-	color: $u-type-info;
-}
+	.grid-text {
+		font-size: 28rpx;
+		margin-top: 4rpx;
+		color: $u-type-info;
+	}
 
-.badge-icon {
-	position: absolute;
-	width: 40rpx;
-	height: 40rpx;
-}
+	.badge-icon {
+		position: absolute;
+		width: 40rpx;
+		height: 40rpx;
+	}
 
-.swiper {
-	height: 480rpx;
-}
+	.swiper {
+		height: 480rpx;
+	}
 
-.indicator-dots {
-	margin-top: 40rpx;
-	display: flex;
-	justify-content: center;
-	align-items: center;
-}
+	.indicator-dots {
+		margin-top: 40rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
 
-.indicator-dots-item {
-	background-color: $u-tips-color;
-	height: 6px;
-	width: 6px;
-	border-radius: 10px;
-	margin: 0 3px;
-}
+	.indicator-dots-item {
+		background-color: $u-tips-color;
+		height: 6px;
+		width: 6px;
+		border-radius: 10px;
+		margin: 0 3px;
+	}
 
-.indicator-dots-active {
-	background-color: $u-type-primary;
-}
+	.indicator-dots-active {
+		background-color: $u-type-primary;
+	}
 </style>
